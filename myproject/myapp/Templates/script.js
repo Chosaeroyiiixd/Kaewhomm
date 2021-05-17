87f4f17175131ceaa35ee80e6e7207a498ea3769
Chosaeroyiiixd <69979384+Chosaeroyiiixd@users.noreply.github.com>
2021-05-17 19:39:01 +0700

    Delete myproject directory

--- myproject/myapp/Templates/script.js
+++
@@ -1,9 +0,0 @@
-const changeBackground = () => {
-    if(window.scrollY >= 80){
-        setnav(true)
-    }else{
-        setnav(false)
-    }
-};
-
-window.addEventListener('scroll', changeBackground);
